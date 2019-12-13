baab0a2e20bed996b2fe5535e6ace3a46ba261ba
cherucole <cheruyotcheruyot@gmail.com>
2019-12-13 13:40:49 +0300

    linking screens

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,14 +1,59 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput
+} from "react-native";
+
+import Colors from "../constants/Colors";
 
 const NewPlaceScreen = props => {
+  const [titleValue, setTitleValue] = useState("");
+
+  const titleChangeHandler = text => {
+    setTitleValue(text);
+  };
+
+  const savePlaceHandler = () =>{
+      
+  }
   return (
-    <View>
-      <Text>New Place Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={titleValue}
+        />
+        <Button title="Save Place" color={Colors.primary} onPress={savePlaceHandler} />
+      </View>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+NewPlaceScreen.navigationOptions = {
+  headerTitle: "Add Place"
+};
+
+const styles = StyleSheet.create({
+  form: {
+    margin: 30
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput: {
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2
+  }
+});
 
 export default NewPlaceScreen;
