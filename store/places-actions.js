347ec67dd9e9183aea703c0456a8ee7c830a2852
cherucole <cheruyotcheruyot@gmail.com>
2019-12-15 18:00:10 +0300

    now storing data and paths on sqlite db

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,21 +1,33 @@
 import * as FileSystem from "expo-file-system";
 
 export const ADD_PLACE = "ADD_PLACE";
+import { insertPlace } from "../helpers/db";
 
 export const addPlace = (title, image) => {
   return async dispatch => {
     const fileName = image.split("/").pop();
     const newPath = FileSystem.documentDirectory + fileName;
+
     try {
       await FileSystem.moveAsync({
         from: image,
         to: newPath
       });
+      const dbResult = await insertPlace(
+        title,
+        newPath,
+        "Dummy Address",
+        12.3,
+        14.8
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_PLACE,
+        placeData: { id: dbResult.insertId, title: title, image: newPath }
+      });
     } catch (err) {
       console.log(err);
       throw err;
     }
-
-    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
   };
 };
